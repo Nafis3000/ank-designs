38ca6dd203642dc60152a5f2a68da1e9ed818fd0
Nafis.3000 <nafis.3000@gmail.com>
2023-04-24 19:31:55 -0600

    fix/test

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 /** @type {import('tailwindcss').Config} */
+const { fontFamily } = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-montserrat)'],
+        sans: ['var(--font-montserrat)', ...fontFamily.sans],
       }
     },
   },
