4f5650aca8e56ba584dd592e0adc779400d48446
Nafis.3000 <nafis.3000@gmail.com>
2023-07-21 15:47:25 -0600

    resume updated

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useState } from "react";
+import React, { useState } from "react";
 import Image from "next/image";
 import Link from "next/link";
 import { AiOutlineClose, AiOutlineMenu, AiOutlineMail, AiOutlineLinkedin, AiOutlineInstagram } from "react-icons/ai";
